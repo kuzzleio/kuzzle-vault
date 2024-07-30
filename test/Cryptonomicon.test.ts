4c67ff602531c38b0dfffa326aaf6d7635393dc2
rolljee <n.riquelmebareiro@gmail.com>
2024-07-30 16:36:37 +0200

    refactor(eslint): use kuzzle eslint plugin

--- test/Cryptonomicon.test.ts
+++ test/Cryptonomicon.test.ts
@@ -1,9 +1,10 @@
-const should = require('should');
+// eslint-disable-next-line
+const should = require("should");
 
-import Cryptonomicon from '../src/Cryptonomicon'
+import Cryptonomicon from "../src/Cryptonomicon";
 
-describe('Cryptonomicon', () => {
-  const vaultKey = 'the spoon does not exists';
+describe("Cryptonomicon", () => {
+  const vaultKey = "the spoon does not exists";
   let decryptedSecrets: any;
   let encryptedSecrets: any;
   let cryptonomicon: Cryptonomicon;
@@ -11,60 +12,63 @@ describe('Cryptonomicon', () => {
   beforeEach(() => {
     decryptedSecrets = {
       aws: {
-        keyId: 'key id',
-        secretKey: 'very long key 1234567890 1234567890 1234567890'
+        keyId: "key id",
+        secretKey: "very long key 1234567890 1234567890 1234567890",
       },
-      deep: { nested: { value: 'nested value' } }
+      deep: { nested: { value: "nested value" } },
     };
 
     encryptedSecrets = {
       aws: {
-        keyId: 'da4e9dcf3b20ae3901211764c97b954b.4000c14b024ca76b48d922d666624eab',
-        secretKey: '7160329ef751a3377354586db9515173991276f5216f73b0789af214c8298f877b08ef6305c1b670c48687d5f2867bb0.d8027bacfeedb64ce54b2d13dd5558bc'
+        keyId:
+          "da4e9dcf3b20ae3901211764c97b954b.4000c14b024ca76b48d922d666624eab",
+        secretKey:
+          "7160329ef751a3377354586db9515173991276f5216f73b0789af214c8298f877b08ef6305c1b670c48687d5f2867bb0.d8027bacfeedb64ce54b2d13dd5558bc",
       },
       deep: {
         nested: {
-          value: 'be2698fc2840a5d4eec839c5a5963b98.97492d612828b6a1974827a9781c6d35'
-        }
-      }
+          value:
+            "be2698fc2840a5d4eec839c5a5963b98.97492d612828b6a1974827a9781c6d35",
+        },
+      },
     };
 
     cryptonomicon = new Cryptonomicon(vaultKey);
   });
 
-  describe('decryptString', () => {
-    it('should decrypt a string', () => {
+  describe("decryptString", () => {
+    it("should decrypt a string", () => {
       const decrypted = cryptonomicon.decryptString(encryptedSecrets.aws.keyId);
 
       should(decrypted).be.eql(decryptedSecrets.aws.keyId);
     });
 
-    it('should raise an error on invalid encrypted string format', () => {
+    it("should raise an error on invalid encrypted string format", () => {
       should(() => {
-        cryptonomicon.decryptString('invalid.')
+        cryptonomicon.decryptString("invalid.");
       }).throw();
 
       should(() => {
-        cryptonomicon.decryptString('.invalid')
+        cryptonomicon.decryptString(".invalid");
       }).throw();
 
       should(() => {
-        cryptonomicon.decryptString('valid.too-short-iv')
+        cryptonomicon.decryptString("valid.too-short-iv");
       }).throw();
     });
   });
 
-  describe('encryptString', () => {
-    it('should encrypt a string in correct format', () => {
+  describe("encryptString", () => {
+    it("should encrypt a string in correct format", () => {
       const encrypted = cryptonomicon.encryptString(decryptedSecrets.aws.keyId);
 
-      const [data, iv] = encrypted.split('.');
+      const [data, iv] = encrypted.split(".");
 
       should(data).not.be.empty();
-      should(Buffer.from(iv, 'hex').length).be.eql(16);
+      should(Buffer.from(iv, "hex").length).be.eql(16);
     });
 
-    it('should encrypt correctly', () => {
+    it("should encrypt correctly", () => {
       const encrypted = cryptonomicon.encryptString(decryptedSecrets.aws.keyId);
       const decrypted = cryptonomicon.decryptString(encrypted);
 
@@ -72,8 +76,8 @@ describe('Cryptonomicon', () => {
     });
   });
 
-  describe('decryptObject', () => {
-    it('should decrypt string values of an object', () => {
+  describe("decryptObject", () => {
+    it("should decrypt string values of an object", () => {
       const decrypted = cryptonomicon.decryptObject(encryptedSecrets);
 
       should(decrypted).be.eql(decryptedSecrets);
--- test/Vault.test.ts
+++ test/Vault.test.ts
@@ -1,16 +1,17 @@
-import * as crypto from 'crypto';
-import * as mockfs from 'mock-fs'
+import * as crypto from "crypto";
+import * as mockfs from "mock-fs";
 
-import Vault from '../src/Vault'
+import Vault from "../src/Vault";
 
-const should = require('should');
-require('should-sinon');
+// eslint-disable-next-line
+const should = require("should");
+require("should-sinon");
 
 const keyHash = (str: string): Buffer => {
-  return Buffer.from(crypto.createHash('sha256').update(str).digest());
+  return Buffer.from(crypto.createHash("sha256").update(str).digest());
 };
 
-describe('Vault', () => {
+describe("Vault", () => {
   let encryptedSecrets: any;
   let decryptedSecrets: any;
   let vault: Vault;
@@ -19,72 +20,76 @@ describe('Vault', () => {
   beforeEach(() => {
     decryptedSecrets = {
       aws: {
-        keyId: 'key id',
-        secretKey: 'very long key 1234567890 1234567890 1234567890'
+        keyId: "key id",
+        secretKey: "very long key 1234567890 1234567890 1234567890",
       },
-      deep: { nested: { value: 'nested value' } }
+      deep: { nested: { value: "nested value" } },
     };
 
-    vaultKey = 'the spoon does not exists';
+    vaultKey = "the spoon does not exists";
 
     encryptedSecrets = {
       aws: {
-        keyId: 'da4e9dcf3b20ae3901211764c97b954b.4000c14b024ca76b48d922d666624eab',
-        secretKey: '7160329ef751a3377354586db9515173991276f5216f73b0789af214c8298f877b08ef6305c1b670c48687d5f2867bb0.d8027bacfeedb64ce54b2d13dd5558bc'
+        keyId:
+          "da4e9dcf3b20ae3901211764c97b954b.4000c14b024ca76b48d922d666624eab",
+        secretKey:
+          "7160329ef751a3377354586db9515173991276f5216f73b0789af214c8298f877b08ef6305c1b670c48687d5f2867bb0.d8027bacfeedb64ce54b2d13dd5558bc",
       },
       deep: {
         nested: {
-          value: 'be2698fc2840a5d4eec839c5a5963b98.97492d612828b6a1974827a9781c6d35'
-        }
-      }
+          value:
+            "be2698fc2840a5d4eec839c5a5963b98.97492d612828b6a1974827a9781c6d35",
+        },
+      },
     };
 
     mockfs({
-      '/secrets.enc.json': JSON.stringify(encryptedSecrets)
+      "/secrets.enc.json": JSON.stringify(encryptedSecrets),
     });
   });
 
-
-  describe('#constructor', () => {
-    it('should use the vault key in parameter', () => {
+  describe("#constructor", () => {
+    it("should use the vault key in parameter", () => {
       vault = new Vault(vaultKey);
 
       should(vault.cryptonomicon?.vaultKeyHash).be.eql(keyHash(vaultKey));
     });
 
-    it('should use env variable if provided', () => {
-      process.env.KUZZLE_VAULT_KEY = 'bend your reality';
+    it("should use env variable if provided", () => {
+      process.env.KUZZLE_VAULT_KEY = "bend your reality";
 
       vault = new Vault(vaultKey);
 
-      should(vault.cryptonomicon?.vaultKeyHash).be.eql(keyHash('bend your reality'));
+      should(vault.cryptonomicon?.vaultKeyHash).be.eql(
+        keyHash("bend your reality"),
+      );
       should(process.env.KUZZLE_VAULT_KEY).be.undefined();
     });
   });
 
-  describe('#decrypt', () => {
+  describe("#decrypt", () => {
     beforeEach(() => {
-      vault = new Vault('the spoon does not exists');
+      vault = new Vault("the spoon does not exists");
     });
 
-    it('should decrypt the secrets', () => {
-      vault.decrypt('/secrets.enc.json');
+    it("should decrypt the secrets", () => {
+      vault.decrypt("/secrets.enc.json");
 
       should(vault.secrets).be.eql(decryptedSecrets);
     });
 
-    it('should throw if no vault key has been provided', () => {
+    it("should throw if no vault key has been provided", () => {
       vault = new Vault();
 
       should(() => {
-        vault.decrypt('/secrets.enc.json');
+        vault.decrypt("/secrets.enc.json");
       }).throw();
     });
 
-    it('should throw if the vault file does not exists', () => {
+    it("should throw if the vault file does not exists", () => {
       should(() => {
-        vault.decrypt('/other/secrets.enc.json');
+        vault.decrypt("/other/secrets.enc.json");
       }).throw();
     });
-  })
+  });
 });
